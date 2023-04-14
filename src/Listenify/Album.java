4d7c20f42dd5a252646995ad72d55554bbbbef74
jainpranshu21 <pranshujain80@gmail.com>
2023-04-14 22:57:13 +0530

    adding song & album

--- src/Listenify/Album.java
+++ src/Listenify/Album.java
@@ -1,4 +1,57 @@
 package Listenify;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Album {
+    public String albumName;
+    public String artistName;
+    public List<Song> songList;
+
+    public Album(String s, String arijit_singh) {
+        this.albumName = albumName;
+        this.artistName = artistName;
+        this.songList= new ArrayList<>();
+    }
+
+    boolean findSongInAlbum(String title){
+        for(Song song:songList) {
+            if (song.title.equals(title))
+                return true;
+        }
+        return false;
+    }
+
+    public String addSongToAlbum(String title,double duration){
+        if(findSongInAlbum(title)==true)
+            return "Song is already present";
+        else{
+            Song newSong=new Song(title,duration);
+            songList.add(newSong);
+            return "New song has been added successfully";
+        }
+    }
+
+    public String addSongToPlayList(int trackNo, LinkedList<Song> playList){
+        int index=trackNo-1;
+        if(index>=0 && index<this.songList.size()){
+            Song song=this.songList.get(index);
+            playList.add(song);
+            return "Song added successfully to playlist";
+        }
+        return "Invalid trackNo";
+    }
+
+
+    public String addSongToPlayList(String title,LinkedList<Song> playList) {
+        for(Song song:songList){
+            if(song.title.equals(title)){
+               playList.add(song);
+               return "Song has been added successfully tp playlist";
+            }
+        }
+        return "Song does not exist in songlist";
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import Listenify.Album;
+import Listenify.Song;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
+    public static List<Album> albums=new ArrayList<>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      Album album=new Album("Old Hindi Song","Arijit Singh");
+
+      album.addSongToAlbum("Pathaan",4.5);
+        album.addSongToAlbum("Channa mereya",3.5);
+        album.addSongToAlbum("Naina",5.0);
+
+        albums.add(album);
+
+        album=new Album("New Songs","Prateek kuhad");
+
+        album.addSongToAlbum("Rap god",4.5);
+        album.addSongToAlbum("Not Afraid",3.5);
+        album.addSongToAlbum("Lose Yourself",4.5);
+
+        albums.add(album);
+
+        LinkedList<Song> playList_1=new LinkedList<>();
+
+        albums.get(0).addSongToPlayList("Pathaan",playList_1);
+        albums.get(0).addSongToPlayList("Naina",playList_1);
+        albums.get(1).addSongToPlayList("Rao god",playList_1);
+        albums.get(1).addSongToPlayList("Not Afraid",playList_1);
+
+
     }
 }
